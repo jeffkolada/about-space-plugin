01316204d68ac9e3af962a5ca798af696abb6bb8
jeffkolada <127796325+jeffkolada@users.noreply.github.com>
2023-04-05 17:22:34 -0700

    add admin panel with fields that do nothing

--- about-space/src/index.js
+++ about-space/src/index.js
@@ -26,6 +26,18 @@ export default class MyPlugin extends BasePlugin {
         })
 
     }
+    this.menus.register({
+        id: 'vatominc-popup-message-config',
+        section: 'plugin-settings',
+        panel: {
+            fields: [
+                { type: 'section', name: 'About Space' },
+                { type: 'text', id: 'about-title', name: 'Title', help: 'Title of your space' },
+                { type: 'textarea', id: 'about-description', name: 'Description', help: 'A short description about your space'},
+                { type: 'text', id: 'text', name: 'Text', help: 'Text to display in the message.' }
+            ]}
+        })
+
 
     /** Called when the user presses the action button */
     onButtonPress() {
@@ -34,7 +46,7 @@ export default class MyPlugin extends BasePlugin {
         this.menus.displayPopup({
             title: 'About Space',
             panel: {
-                iframeURL: this.paths.absolute('popup.html'),
+                iframeURL: this.paths.absolute('./popup.html'),
                 width: 1200,
                 height: 800
             }
